6f884e6133b4fc8169adfa3436e208e7ccf8c33c
Nick Harvey <nickharveyonline@gmail.com>
2016-02-04 14:39:50 -0500

    updating

--- platforms/android/assets/www/js/config.js
+++ platforms/android/assets/www/js/config.js
@@ -1,4 +1,4 @@
-angular.module('your_app_name.config', [])
+angular.module('SKOApp.config', [])
 .constant('WORDPRESS_API_URL', 'http://wordpress-niharvey.rhcloud.com/api/')
 .constant('GCM_SENDER_ID', '574597432927')
 
