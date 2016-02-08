a55e23827ee1698cc4570dc8966316a774caefa1
Nick Harvey <nickharveyonline@gmail.com>
2016-02-08 15:52:04 -0500

    added in tracks

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,6 +1,47 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
+        "id": "ionic-plugin-keyboard.keyboard",
+        "pluginId": "ionic-plugin-keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "pluginId": "com.phonegap.plugins.PushPlugin",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/com.rjfun.cordova.iad/www/iAd.js",
+        "id": "com.rjfun.cordova.iad.iAd",
+        "pluginId": "com.rjfun.cordova.iad",
+        "clobbers": [
+            "window.iAd"
+        ]
+    },
+    {
+        "file": "plugins/com.synconset.imagepicker/www/imagepicker.js",
+        "id": "com.synconset.imagepicker.ImagePicker",
+        "pluginId": "com.synconset.imagepicker",
+        "clobbers": [
+            "plugins.imagePicker"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
         "id": "cordova-plugin-console.console",
         "pluginId": "cordova-plugin-console",
@@ -17,36 +58,11 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
-        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
-        "pluginId": "nl.x-services.plugins.socialsharing",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
         "clobbers": [
-            "window.plugins.socialsharing"
-        ]
-    },
-    {
-        "file": "plugins/com.rjfun.cordova.iad/www/iAd.js",
-        "id": "com.rjfun.cordova.iad.iAd",
-        "pluginId": "com.rjfun.cordova.iad",
-        "clobbers": [
-            "window.iAd"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
-        "id": "cordova-plugin-admobpro.AdMob",
-        "pluginId": "cordova-plugin-admobpro",
-        "clobbers": [
-            "window.AdMob"
-        ]
-    },
-    {
-        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
-        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
-        "pluginId": "de.appplant.cordova.plugin.email-composer",
-        "clobbers": [
-            "cordova.plugins.email",
-            "plugin.email"
+            "device"
         ]
     },
     {
@@ -56,53 +72,6 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
-        "id": "cordova-plugin-globalization.GlobalizationError",
-        "pluginId": "cordova-plugin-globalization",
-        "clobbers": [
-            "window.GlobalizationError"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
-        "id": "cordova-plugin-globalization.globalization",
-        "pluginId": "cordova-plugin-globalization",
-        "clobbers": [
-            "navigator.globalization"
-        ]
-    },
-    {
-        "file": "plugins/org.pushandplay.cordova.apprate/www/AppRate.js",
-        "id": "org.pushandplay.cordova.apprate.AppRate",
-        "pluginId": "org.pushandplay.cordova.apprate",
-        "clobbers": [
-            "AppRate"
-        ]
-    },
-    {
-        "file": "plugins/org.pushandplay.cordova.apprate/www/locales.js",
-        "id": "org.pushandplay.cordova.apprate.locales",
-        "pluginId": "org.pushandplay.cordova.apprate",
-        "runs": true
-    },
-    {
-        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
-        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
-        "pluginId": "com.phonegap.plugins.PushPlugin",
-        "clobbers": [
-            "PushNotification"
-        ]
-    },
-    {
-        "file": "plugins/ionic-plugin-keyboard/www/ios/keyboard.js",
-        "id": "ionic-plugin-keyboard.keyboard",
-        "pluginId": "ionic-plugin-keyboard",
-        "clobbers": [
-            "cordova.plugins.Keyboard"
-        ],
-        "runs": true
     },
     {
         "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
@@ -137,31 +106,99 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
+        "id": "cordova-plugin-globalization.GlobalizationError",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "window.GlobalizationError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-globalization/www/globalization.js",
+        "id": "cordova-plugin-globalization.globalization",
+        "pluginId": "cordova-plugin-globalization",
+        "clobbers": [
+            "navigator.globalization"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-googleplus/www/GooglePlus.js",
+        "id": "cordova-plugin-googleplus.GooglePlus",
+        "pluginId": "cordova-plugin-googleplus",
+        "clobbers": [
+            "window.plugins.googleplus"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
         "id": "cordova-plugin-splashscreen.SplashScreen",
         "pluginId": "cordova-plugin-splashscreen",
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
+        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
+        "pluginId": "de.appplant.cordova.plugin.email-composer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "pluginId": "nl.x-services.plugins.socialsharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "file": "plugins/org.pushandplay.cordova.apprate/www/AppRate.js",
+        "id": "org.pushandplay.cordova.apprate.AppRate",
+        "pluginId": "org.pushandplay.cordova.apprate",
+        "clobbers": [
+            "AppRate"
+        ]
+    },
+    {
+        "file": "plugins/org.pushandplay.cordova.apprate/www/locales.js",
+        "id": "org.pushandplay.cordova.apprate.locales",
+        "pluginId": "org.pushandplay.cordova.apprate",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-console": "1.0.2",
-    "nl.x-services.plugins.socialsharing": "4.3.19",
+    "ionic-plugin-keyboard": "1.0.8",
+    "com.phonegap.plugins.PushPlugin": "2.5.0",
     "cordova-plugin-extension": "1.2.3",
     "com.rjfun.cordova.iad": "2.0.4",
+    "com.synconset.imagepicker": "1.0.7",
     "cordova-plugin-admobpro": "2.10.0",
+    "cordova-plugin-console": "1.0.2",
+    "cordova-plugin-device": "1.1.0",
+    "cordova-plugin-dialogs": "1.2.0",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-globalization": "1.0.2",
+    "cordova-plugin-googleplus": "4.0.7",
+    "cordova-plugin-inappbrowser": "1.1.2-dev",
+    "cordova-plugin-splashscreen": "2.1.0",
     "cordova-plugin-whitelist": "1.2.0",
     "de.appplant.cordova.plugin.email-composer": "0.8.3dev",
-    "cordova-plugin-dialogs": "1.2.0",
-    "cordova-plugin-globalization": "1.0.2",
-    "org.pushandplay.cordova.apprate": "1.1.7",
-    "com.phonegap.plugins.PushPlugin": "2.5.0",
-    "ionic-plugin-keyboard": "1.0.8",
-    "cordova-plugin-geolocation": "1.0.1",
-    "cordova-plugin-splashscreen": "2.1.0"
+    "nl.x-services.plugins.socialsharing": "4.3.19",
+    "org.pushandplay.cordova.apprate": "1.1.7"
 }
 // BOTTOM OF METADATA
 });
